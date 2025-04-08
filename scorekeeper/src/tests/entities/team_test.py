58e8999aa253dfd7e656737651373514b9cdb815
Arttu Kankaanpää <arttu.kankaanpaa@gmail.com>
2025-04-08 15:17:25 +0300

    added tests

---
+++ scorekeeper/src/tests/entities/team_test.py
@@ -0,0 +1,21 @@
+import unittest
+from src.entities.team import Team
+from src.entities.player import Player
+
+
+class TestTeam(unittest.TestCase):
+    def setUp(self):
+        self.test_team = Team("test_team")
+
+    def test_add_player(self):
+        test_player = Player("test_player")
+        self.test_team.add_player(test_player)
+        self.assertEqual(self.test_team.players[0], test_player)
+
+    def test_get_players(self):
+        test_player1 = Player("test_player1")
+        test_player2 = Player("test_player2")
+        self.test_team.add_player(test_player1)
+        self.test_team.add_player(test_player2)
+        self.assertEqual(self.test_team.get_players(),
+                         [test_player1, test_player2])
--- scorekeeper/src/tests/services/scorekeeper_test.py
+++ scorekeeper/src/tests/services/scorekeeper_test.py
@@ -1,5 +1,6 @@
 import unittest
 from src.services.score_service import ScoreService
+from entities.team import Team
 
 
 class TestScoreService(unittest.TestCase):
@@ -15,3 +16,16 @@ class TestScoreService(unittest.TestCase):
         self.assertEqual(len(events), 1)
         self.assertEqual(events[0].content, 2)
         self.assertEqual(events[0].type, "basket")
+
+    def test_add_team(self):
+        test_team = Team("test")
+        self.score_service.add_team(test_team)
+        self.assertEqual(self.score_service.teams[0], test_team)
+
+    def test_get_teams(self):
+        test_team1 = Team("test1")
+        test_team2 = Team("test2")
+        self.score_service.add_team(test_team1)
+        self.score_service.add_team(test_team2)
+        self.assertEqual(self.score_service.get_teams(),
+                         [test_team1, test_team2])
